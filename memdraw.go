ad361d5fd7d5b7371bd95e2bdd9565310974e1fb
as <as.utf8@gmail.com>
2017-11-28 00:15:31 -0800

    flatten curve

--- memdraw.go
+++ memdraw.go
@@ -6,8 +6,7 @@ package memdraw
 import (
 	"image"
 	"image/draw"
-
-//	"github.com/as/frame/font"
+	//	"github.com/as/frame/font"
 )
 
 // Border draws an outline of a rectangle on dst
@@ -20,6 +19,8 @@ func Border(dst draw.Image, r image.Rectangle, thick int, sp image.Point, src im
 
 // Line draws a line from q0 to q1 on dst
 func Line(dst draw.Image, q0, q1 image.Point, thick int, src image.Image, sp image.Point) {
+	// Note: There exists a 4-step and 8-step optimization that takes advantage of repeating patterns in the
+	// line. Is it worth it?
 	if q1.X < q0.X {
 		q0, q1 = q1, q0
 	}
@@ -51,7 +52,6 @@ func Line(dst draw.Image, q0, q1 image.Point, thick int, src image.Image, sp ima
 			p0.X++
 			d += dy
 		}
-
 	} else {
 		d := 2*dx - dy
 		for p0.Y < dy {
@@ -84,39 +84,135 @@ func Poly(dst draw.Image, p []image.Point, end0, end1, thick int, src image.Imag
 // and end1; the thickness of the curve is 1+2*thick.
 // The source is aligned so sp in src corresponds to a in dst.
 func Bezier(dst draw.Image, a, b, c, d image.Point, end0, end1, thick int, src image.Image, sp image.Point) {
+	// Start with a simple, but extemely slow implementation. Optimize it later when time permits
+	flatcurve(dst, []image.Point{a, b, c, d}, thick, src, sp)
+}
 
-	// start with a slow implementation and 
-	// optimize it later when time permits
-	for t := float64(0); t < 1.0; t+= 0.005{
-		curve(dst, []image.Point{a,b,c,d}, t, thick, src, sp)
+func flatcurve(dst draw.Image, p []image.Point, thick int, src image.Image, sp image.Point) {
+	const (
+		seg  = 100.0
+		step = 1 / seg
+	)
+	t := 0.0
+	q := make([]image.Point, 0, seg+2)
+	q = append(q, p[0])
+	for i := 0; i < seg; i++ {
+		t = float64(i) * step
+		q = append(q, curve(dst, p, t, thick, src, sp))
 	}
+	q = append(q, p[len(p)-1])
+	Poly(dst, q, 1, 1, thick, src, sp)
 }
 
-func flatcurve(dst draw.Image, p []image.Point, thick int, src image.Image, sp image.Point){
-	
+func N(t, i, k int) (n int) {
+	type tik struct {
+		t, i, k int
+	}
+	var cache map[tik]int
+	func() {
+		if cache == nil {
+			cache = make(map[tik]int)
+			return
+		}
+	}()
+	if cN, ok := cache[tik{t, i, k}]; ok {
+		return cN
+	}
+	defer func() {
+		cache[tik{t, i, k}] = n
+	}()
+	gt := func(i int) int {
+		return N(t, i, k)
+	}
+	if k == 1 {
+		if gt(i) <= t && t <= gt(i+1) {
+			return 1
+		}
+		return 0
+	}
+	return ((t-gt(i))*N(t, i, k-1))/
+		(gt(i+k-1)-gt(i)) + ((gt(i+k)-t)*N(t, i+1, k-1))/
+		(gt(i+k)-gt(i+1))
 }
 
-func curve(dst draw.Image, p []image.Point, t float64, thick int, src image.Image, sp image.Point){
-	if len(p) == 1{
-		r := image.Rect(-1,-1,1,1).Inset(-thick+1).Add(p[0])
-		draw.Draw(dst, r, src, sp, draw.Src)
-		return
+func curve(dst draw.Image, p []image.Point, t float64, thick int, src image.Image, sp image.Point) image.Point {
+	if len(p) == 1 {
+		//r := image.Rect(-1,-1,1,1).Inset(-thick+1).Add(p[0])
+		//draw.Draw(dst, r, src, sp, draw.Src)
+		return p[0]
 	}
 	p2 := make([]image.Point, 0, len(p)-1)
-	for i := 0; i < len(p)-1; i++{
-		x := int((1-t) * float64(p[i].X) + t * float64(p[i+1].X))
-		y := int((1-t) * float64(p[i].Y) + t * float64(p[i+1].Y))
-		p2 = append(p2, image.Pt(x,y))
+	for i := 0; i < len(p)-1; i++ {
+		x := int((1-t)*float64(p[i].X) + t*float64(p[i+1].X))
+		y := int((1-t)*float64(p[i].Y) + t*float64(p[i+1].Y))
+		p2 = append(p2, image.Pt(x, y))
 	}
-	curve(dst, p2, t, thick, src, sp)
+	return curve(dst, p2, t, thick, src, sp)
 }
 
 // Bezspline takes the same arguments as poly but draws a
 // quadratic B-spline (despite its name) rather than a
 // polygon. If the first and last points in p are equal,
 // the spline has periodic end conditions.
-func Bezspline(dst draw.Image, p []image.Point, end0, end1, thick int, src image.Image, sp image.Point) {
+func Bezspline(dst draw.Image, a, b, c, d image.Point, end0, end1, thick int, src image.Image, sp image.Point) {
+	p := []image.Point{a, b, c, d}
+	//	for t := float64(0); t < 1.0; t+= 0.1{
+	//		curve(dst, interp(pt, t), t, thick, src, sp)
+	//	}
+	const (
+		seg  = 100.0
+		step = 1 / seg
+	)
+	t := 0.0
+	q := make([]image.Point, 0, seg+2)
+	for i := 0; i < seg; i++ {
+		t = float64(i) * step
+		q = append(q, curve(dst, interp(p, t), t, thick, src, sp))
+	}
+	Poly(dst, q, 1, 1, thick, src, sp)
+}
 
+func interp(p []image.Point, t float64) []image.Point {
+	deg := 2
+	n := len(p)
+	ndeg := len(p) - 1 - deg
+
+	w := make([]float64, n)
+	for i := range w {
+		w[i] = 1.0
+	}
+
+	k := make([]float64, len(p)+2)
+	for i := range k {
+		k[i] = float64(i)
+	}
+
+	lo, hi := float64(deg), float64(ndeg)
+	t *= hi - lo
+	t += lo
+
+	q := make([]image.Point, len(p))
+	for i := range p {
+		q[i] = p[i]
+		q[i].X = int(float64(q[i].X) * float64(w[i]))
+		q[i].Y = int(float64(q[i].Y) * float64(w[i]))
+	}
+
+	s := deg
+	for ; s < ndeg; s++ {
+		if t >= k[s] && t <= k[s+1] {
+			break
+		}
+	}
+
+	for l := 1; l <= deg+1; l++ {
+		for i := s; i > s-deg+l; i-- {
+			a := (t - k[i]) / (k[i+deg+1-l] - k[i])
+			q[i].X = int((1-a)*float64(q[i-1].X) + a*float64(q[i].X))
+			q[i].Y = int((1-a)*float64(q[i-1].Y) + a*float64(q[i].Y))
+		}
+	}
+	return q
 }
 
 // Ellipse draws a filled ellipse at center point c
