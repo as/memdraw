29e2fcb7ecf22908171b82a3a48a3a43a46b55a7
as <as.utf8@gmail.com>
2018-09-16 18:51:16 -0700

    example: clean up

--- example/harness/harness.go
+++ example/harness/harness.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"image/color/palette"
@@ -10,6 +11,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/as/frame"
 	"github.com/as/memdraw"
 	"github.com/as/shiny/event/key"
 	"github.com/as/shiny/event/lifecycle"
@@ -19,7 +21,7 @@ import (
 )
 
 var (
-	size   = image.Pt(1024, 768)
+	size   = image.Pt(1024, 868)
 	w      = dev.Window()
 	dev, _ = ui.Init(&ui.Config{
 		Width: size.X, Height: size.Y,
@@ -38,12 +40,12 @@ func blank() {
 	}
 	draw.Draw(fb, fb.Bounds(), image.Black, image.ZP, draw.Src)
 }
-func paint() {
-	w.Upload(image.ZP, img, img.Bounds())
-	w.Publish()
-	if recording {
-		capture(img.RGBA())
-	}
+func redraw() {
+			w.Upload(image.ZP, img, img.Bounds())
+			w.Publish()
+			if recording {
+				capture(img.RGBA())
+			}
 }
 
 var (
@@ -75,14 +77,15 @@ func grab(p image.Point, list []image.Point) bool {
 func main() {
 	d := screen.Dev
 	blank()
-	paint()
+	redraw()
 
 	var (
 		lpts = make([]image.Point, 2)
-		pts  = make([]image.Point, 128)
+		pts  = make([]image.Point, 5)
 	)
 
-	hz := time.NewTicker(time.Second / 30).C
+	hz := time.NewTicker(time.Second / 128).C
+	fr := frame.New(fb, image.Rect(0, 768, 1024, 868), nil)
 
 	for {
 		select {
@@ -94,17 +97,17 @@ func main() {
 				lpts[0] = pts[1]
 				lpts[1] = pt(e)
 				memdraw.Line(fb, lpts[0], lpts[1], 1, blue, image.ZP)
-				paint()
+				redraw()
 			case HasButton(2, down):
 				select {
 				case <-hz:
-					//drawCurve(fb, black, pts...)
+					// drawCurve(fb, black, pts...)
 
 					copy(pts, pts[1:])
 					pts[len(pts)-1] = pt(e)
-
+					
 					drawCurve(fb, next(), pts...)
-					paint()
+					redraw()
 				default:
 				}
 			case HasButton(1, down):
@@ -119,7 +122,7 @@ func main() {
 					drawCurve(fb, black, pts...)
 					pts[current] = pt(e)
 					drawCurve(fb, uniform, pts...)
-					paint()
+					redraw()
 				}
 			}
 		case e := <-d.Key:
@@ -129,9 +132,12 @@ func main() {
 			if specialkey(e.Rune) {
 				continue
 			}
+			blank()
 			switch e.Code {
 			case key.CodeUpArrow:
 				pts = append(pts, pts[len(pts)-1])
+				fr.Delete(0, fr.Len())
+				fr.Insert([]byte(fmt.Sprintf("Degree: %d\n", len(pts)-1)), 0)
 			case key.CodeDownArrow:
 				if len(pts)-1 != 0 {
 					pts = pts[:len(pts)-1]
@@ -139,15 +145,16 @@ func main() {
 						current--
 					}
 				}
+				fr.Delete(0, fr.Len())
+				fr.Insert([]byte(fmt.Sprintf("Degree: %d\n", len(pts)-1)), 0)
 			}
-			blank()
 			drawCurve(fb, uniform, pts...)
-			paint()
+			redraw()
 		case e := <-d.Size:
 			size = image.Pt(e.WidthPx, e.HeightPx)
 		case e := <-d.Paint:
 			e = e
-			paint()
+			redraw()
 		case e := <-d.Lifecycle:
 			if e.To == lifecycle.StageDead {
 				os.Exit(0)
@@ -182,9 +189,15 @@ func init() {
 	}
 }
 
-func capture(img image.Image) {
-	r := image.Rectangle{image.ZP, camera.Bounds().Size()}
-	draw.Draw(&raw[fp], r, img, camera.Bounds().Min, draw.Src)
+func capture(img *image.RGBA) {
+	rmin := img.Rect.Min
+	s := camera.Bounds().Min
+	sp := (s.Y-rmin.Y)*img.Stride + (s.X-rmin.X)*4
+	e := camera.Bounds().Max
+	ep := (e.Y-rmin.Y)*img.Stride + (e.X-rmin.X)*4
+	copy(raw[fp].Pix, img.Pix[sp:ep])
+
+	// draw.Draw(&raw[fp], r, img, camera.Bounds().Min, draw.Src)
 	fp++
 	if fp == len(pending) {
 		fp--
@@ -208,11 +221,11 @@ func specialkey(r rune) bool {
 			memdraw.Border(fb, camera, 1, image.ZP, black)
 			camera.Max = pt(e)
 			memdraw.Border(fb, camera, 1, image.ZP, red)
-			paint()
+			redraw()
 		}
 		memdraw.Border(fb, camera, 1, image.ZP, black)
 		camera = camera.Canon() // lol
-		paint()
+		redraw()
 	case 's':
 		fp = 0
 		recording = true
@@ -253,13 +266,20 @@ func specialkey(r rune) bool {
  */
 
 func drawCurve(dst draw.Image, src image.Image, p ...image.Point) {
-	//drawControls(dst, src, p...)
+	drawControls(dst, src, p...)
+	// memdraw.Bezspline(fb, p[0],p[1],p[2],p[3],1,1,1, src, image.ZP)
 	memdraw.BezierN(fb, 1, 1, 1, src, dst.Bounds().Min, p...)
 }
 
 func drawControls(dst draw.Image, src image.Image, p ...image.Point) {
 	for _, p := range p {
 		memdraw.Ellipse(dst, p, 3, 3, 1, src, dst.Bounds().Min, 1, 1)
+	}
+}
+
+func drawEllipses(dst draw.Image, src image.Image, c, b, a int, p ...image.Point) {
+	for _, p := range p {
+		memdraw.Ellipse(dst, p, c, b, a, src, dst.Bounds().Min, 1, 1)
 	}
 }
 
@@ -306,12 +326,24 @@ func readmouse(e mouse.Event) mouse.Event {
  */
 
 var rainbow = color.RGBA{255, 0, 0, 255}
+var prime = color.RGBA{255, 0, 0, 255}
 var uniform = image.NewUniform(rainbow)
+var uprime = image.NewUniform(prime)
 
 func next() *image.Uniform {
 	rainbow = nextcolor(rainbow)
+	prime = inverse(rainbow)
 	uniform = image.NewUniform(rainbow)
+	uprime = image.NewUniform(prime)
 	return uniform
+}
+
+// inverse steps through a gradient
+func inverse(c color.RGBA) color.RGBA {
+	c.R = ^c.R - 147
+	c.G = ^c.G - 147
+	c.B = ^c.B - 147
+	return c
 }
 
 // nextcolor steps through a gradient
